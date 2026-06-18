d84dacbc6ce504eaf57924e01d265944efbc69b3
Neramit <DavidTh30@gmail.com>
2026-06-19 02:20:10 +0700

    Add files via upload

--- Turning calculator/unit1.pas
+++ Turning calculator/unit1.pas
@@ -29,6 +29,11 @@ type
     P_Counter_sec:longint;
     I_StopTurningSetpoint: extended;
 
+    I_RampUpGearDelay_ms: longint;
+    P_RampUpGearDelay_ms: longint;
+    I_OffsetTurningMotorPer_ms: extended;
+    P_RampUp:boolean;
+
   end;
 
 type
@@ -38,6 +43,7 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     FloatSpinEdit1: TFloatSpinEdit;
+    FloatSpinEdit2: TFloatSpinEdit;
     HMIPolyline1: THMIPolyline;
     HMIPolyline2: THMIPolyline;
     HMIPolyline3: THMIPolyline;
@@ -49,6 +55,11 @@ type
     Label12: TLabel;
     Label13: TLabel;
     Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
+    Label17: TLabel;
+    Label18: TLabel;
+    Label19: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
@@ -59,11 +70,14 @@ type
     Label9: TLabel;
     SpinEdit1: TSpinEdit;
     SpinEdit2: TSpinEdit;
+    SpinEdit3: TSpinEdit;
     procedure Button1Click(Sender: TObject);
     procedure FloatSpinEdit1EditingDone(Sender: TObject);
+    procedure FloatSpinEdit2EditingDone(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure SpinEdit1EditingDone(Sender: TObject);
     procedure SpinEdit2EditingDone(Sender: TObject);
+    procedure SpinEdit3EditingDone(Sender: TObject);
   private
     procedure OnIdle(Sender: TObject; var Done: boolean);
   public
@@ -97,18 +111,32 @@ begin
     IF(Motor01.I_MotorRun) then
     begin
       Motor01.P_TurningCounter_ms:=Motor01.P_TurningCounter_ms+SetpointLoop_ms;
-      Motor01.O_TurningMotorCounter_ms:=Motor01.O_TurningMotorCounter_ms+Motor01.C_Round_Per_XXXms;
-      Motor01.O_TurningGearCounter_ms:=Motor01.O_TurningGearCounter_ms+Motor01.C_GearRound_Per_XXXms;
+      if (not Motor01.P_RampUp) then Motor01.O_TurningMotorCounter_ms:=Motor01.O_TurningMotorCounter_ms+Motor01.C_Round_Per_XXXms;
+      if (not Motor01.P_RampUp) then Motor01.O_TurningGearCounter_ms:=Motor01.O_TurningGearCounter_ms+Motor01.C_GearRound_Per_XXXms;
       Motor01.P_Counter_ms:=Motor01.P_Counter_ms+SetpointLoop_ms+Motor01.P_Timer_Leftover;
+      if (Motor01.P_RampUp) then Motor01.P_RampUpGearDelay_ms:=Motor01.P_RampUpGearDelay_ms+SetpointLoop_ms+Motor01.P_Timer_Leftover;
       Motor01.P_Timer_Leftover:=0;
+
       if (Motor01.P_Counter_ms>=1000) then
       begin
         Motor01.P_Timer_Leftover:=Motor01.P_Counter_ms-1000;
-        Motor01.P_Counter_ms:=0;
         Motor01.P_Counter_sec:=Motor01.P_Counter_sec+1;
         Motor01.P_TurningCounter_sec:=Motor01.P_TurningCounter_sec+1;
-        Motor01.O_TurningMotorCounter_sec:=Motor01.O_TurningMotorCounter_sec+Motor01.C_Round_Per_sec;
-        Motor01.O_TurningGearCounter_sec:=Motor01.O_TurningGearCounter_sec+Motor01.C_GearRound_Per_sec;
+        Motor01.P_Counter_ms:=0;
+        if (not Motor01.P_RampUp) then Motor01.O_TurningMotorCounter_sec:=Motor01.O_TurningMotorCounter_sec+Motor01.C_Round_Per_sec;
+        if (not Motor01.P_RampUp) then Motor01.O_TurningGearCounter_sec:=Motor01.O_TurningGearCounter_sec+Motor01.C_GearRound_Per_sec;
+      end;
+
+      if ((Motor01.P_RampUpGearDelay_ms>=Motor01.I_RampUpGearDelay_ms) and (Motor01.P_RampUp)) then
+      begin
+        Motor01.P_RampUp:=False;
+        Motor01.P_Timer_Leftover:=Motor01.P_RampUpGearDelay_ms-Motor01.I_RampUpGearDelay_ms;
+        Motor01.P_Counter_ms:=0;
+
+        Motor01.O_TurningMotorCounter_ms:=Motor01.I_OffsetTurningMotorPer_ms;
+        Motor01.O_TurningMotorCounter_sec:=Motor01.O_TurningMotorCounter_sec+Motor01.I_OffsetTurningMotorPer_ms;
+        Motor01.O_TurningGearCounter_ms:=Motor01.I_OffsetTurningMotorPer_ms*Motor01.I_GearRatio;
+        Motor01.O_TurningGearCounter_sec:=Motor01.O_TurningGearCounter_sec+Motor01.I_OffsetTurningMotorPer_ms*Motor01.I_GearRatio;
       end;
 
       if (Motor01.P_Counter_sec>=60) then
@@ -170,7 +198,10 @@ begin
     Label7.Caption:='Gear turn/sec ='+FormatFloat('0.00',Motor01.C_GearRound_Per_sec);
     Label8.Caption:='Gear turn/'+SetpointLoop_ms.ToString+'ms ='+FormatFloat('0.00',Motor01.C_GearRound_Per_XXXms);
 
+    Label15.Caption:='Cyc: '+ElapsedTime.ToString+' ms';
+    Label16.Caption:='Left over: '+Timer_Leftover.ToString+' ms';
 
+    Label19.Caption:='ActualOffset_ms = '+Motor01.P_RampUpGearDelay_ms.ToString+' ms';
   end;
   Application.ProcessMessages;
 
@@ -202,6 +233,10 @@ begin
   Motor01.P_Counter_sec:=0;
   Motor01.P_Timer_Leftover:=0;
   Motor01.I_StopTurningSetpoint:=250;
+  Motor01.I_RampUpGearDelay_ms:=500;
+  Motor01.P_RampUpGearDelay_ms:=0;
+  Motor01.I_OffsetTurningMotorPer_ms:=10;
+  Motor01.P_RampUp:=True;
 
 
   Application.OnIdle := @OnIdle;
@@ -211,7 +246,7 @@ end;
 procedure TForm1.SpinEdit1EditingDone(Sender: TObject);
 begin
   SpinEdit1.Value:=Abs(SpinEdit1.Value);
-  Motor01.I_RPM:=Abs(SpinEdit1.Value);
+  Motor01.I_RPM:=SpinEdit1.Value;
 
   Motor01.C_Round_Per_sec:=Motor01.I_RPM/60;
   Motor01.C_Round_Per_XXXms:=Motor01.C_Round_Per_sec/(1000/SetpointLoop_ms);
@@ -224,7 +259,13 @@ end;
 procedure TForm1.SpinEdit2EditingDone(Sender: TObject);
 begin
   SpinEdit2.Value:=Abs(SpinEdit2.Value);
-  Motor01.I_StopTurningSetpoint:=Abs(SpinEdit2.Value);
+  Motor01.I_StopTurningSetpoint:=SpinEdit2.Value;
+end;
+
+procedure TForm1.SpinEdit3EditingDone(Sender: TObject);
+begin
+  SpinEdit3.Value:=Abs(SpinEdit3.Value);
+  Motor01.I_RampUpGearDelay_ms:=SpinEdit3.Value;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -233,6 +274,9 @@ begin
 
   IF(Motor01.I_MotorRun) then
   begin
+    Motor01.P_RampUp:=True;
+    Motor01.P_RampUpGearDelay_ms:=0;
+
     Motor01.O_TurningMotorCounter_ms:=0;
     Motor01.P_Counter_ms:=0;
     Motor01.P_Timer_Leftover:=0;
@@ -255,7 +299,7 @@ end;
 procedure TForm1.FloatSpinEdit1EditingDone(Sender: TObject);
 begin
   FloatSpinEdit1.Value:=Abs(FloatSpinEdit1.Value);
-  Motor01.I_GearRatio:=Abs(FloatSpinEdit1.Value);
+  Motor01.I_GearRatio:=FloatSpinEdit1.Value;
 
   Motor01.C_Round_Per_sec:=Motor01.I_RPM/60;
   Motor01.C_Round_Per_XXXms:=Motor01.C_Round_Per_sec/(1000/SetpointLoop_ms);
@@ -263,6 +307,12 @@ begin
   Motor01.O_TurningMotorCounter_sec:=0;
   Motor01.C_GearRound_Per_sec:=Motor01.C_Round_Per_sec*Motor01.I_GearRatio;
   Motor01.C_GearRound_Per_XXXms:=Motor01.C_GearRound_Per_sec/(1000/SetpointLoop_ms);
+end;
+
+procedure TForm1.FloatSpinEdit2EditingDone(Sender: TObject);
+begin
+  FloatSpinEdit2.Value:=Abs(FloatSpinEdit2.Value);
+  Motor01.I_OffsetTurningMotorPer_ms:=FloatSpinEdit2.Value;
 end;
 
 end.
